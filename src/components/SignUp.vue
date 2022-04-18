9f893aa84172062e66575403e136d96e8749c492
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-04-18 17:11:09 -0500

    Added delay and loader for for name verification

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,8 +1,22 @@
 <template>
   <form @submit.prevent="handleSubmit">
     <h1>Sign Up</h1>
-    <label>Username: </label
-    ><input type="username" required v-model="username" />
+    <label>Username: </label>
+    <div class="flex">
+      <input
+        type="username"
+        required
+        v-model="username"
+        @blur="handleNameChange"
+      />
+      <div class="loader" v-if="requestingName"></div>
+    </div>
+    <div v-if="dataName !== null">
+      <p class="error">{{ errorName }}</p>
+      <p :class="dataName.nameTaken ? 'taken' : 'not-taken'">
+        {{ dataName.msg }}
+      </p>
+    </div>
     <label>Password: </label
     ><input type="password" required v-model="password" />
     <p class="error">{{ passwordError }}</p>
@@ -12,9 +26,12 @@
       <label>Accept terms and conditions</label>
     </div>
 
-    <button type="submit" v :disabled="!username || !password">
-      Create Account
-    </button>
+    <div class="flex">
+      <button type="submit" v :disabled="!username || !password">
+        Create Account
+      </button>
+      <div class="loader" v-if="requesting"></div>
+    </div>
   </form>
 </template>
 
@@ -22,11 +39,11 @@
 import store from "@/store";
 import router from "@/router";
 import { onMounted, ref } from "@vue/runtime-core";
-import { userService } from '@/services/user.services';
+import { userService } from "@/services/user.services";
+import { DelayedCall } from "@/composables/timers/timers";
 export default {
   data() {
     return {
-
       // errors
       passwordError: null,
     };
@@ -41,11 +58,12 @@ export default {
       router.push({ name: "Games" });
     }
 
+    // User Registration
     const username = ref("");
     const password = ref("");
     const terms = ref("");
-
     const { error, load, requesting } = userService.signup();
+
     const handleSubmit = async () => {
       error.value = null;
       await load(username.value, password.value);
@@ -58,7 +76,45 @@ export default {
       }
     };
 
-    return { username, password, terms, error, requesting, handleSubmit };
+    // Username validation
+    const timeId = ref(0);
+    const delay = 500;
+    const {
+      data: dataName,
+      error: errorName,
+      load: loadName,
+      requesting: requestingName,
+    } = userService.nameexists();
+    const nameError = ref("");
+
+    const handleNameChange = async () => {
+      nameError.value = null;
+      clearTimeout(timeId.value, delay);
+      timeId.value = 0;
+      if (username.value.length < 2) return;
+
+      requestingName.value = true;
+      DelayedCall(() => {
+        loadName(username.value);
+        clearTimeout(timeId.value);
+        timeId.value = 0;
+      }, delay);
+    };
+
+    return {
+      username,
+      password,
+      terms,
+      error,
+      requesting,
+      handleSubmit,
+
+      errorName,
+      loadName,
+      requestingName,
+      dataName,
+      handleNameChange,
+    };
   },
 };
 </script>
@@ -133,5 +189,40 @@ button:disabled {
   margin-top: 10px;
   font-size: 0.8em;
   font-weight: bold;
+}
+.taken {
+  color: red;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+.not-taken {
+  color: green;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+.border {
+  border: 1px solid red;
+}
+.loader {
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 16px;
+  height: 16px;
+  animation: spin 2s linear infinite;
+}
+.flex {
+  display: flex;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
